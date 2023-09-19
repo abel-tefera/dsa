75661b0de27300106b519bc955b62e7e9a346178
abel-tefera <abeltefera16@gmail.com>
2023-09-19 09:58:39 +0300

    bubble sort

---
+++ Sorting/bubble.js
@@ -0,0 +1,19 @@
+const bubbleSort = (arr) => {
+    let repeat = true;
+    while (repeat) {
+        repeat = false;
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                repeat = true;
+            }
+        }
+    }
+
+    return arr;
+
+};
+
+console.log(bubbleSort([6, 5, 3, 1, 8, 7, 2, 4]))
